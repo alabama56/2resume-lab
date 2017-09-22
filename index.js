190064672169c9b69258d5d356f35b0750e25479
alabama56 <jakedownard@yahoo.com>
2017-09-22 16:51:06 -0500

    close but no cigar

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
     $('.parallax').parallax();
+    $('.carousel.carousel-slider').on('click', function(){
+        $('.carousel.carousel-slider').carousel('next');
+    });
 });
